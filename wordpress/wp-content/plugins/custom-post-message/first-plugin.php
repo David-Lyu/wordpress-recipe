d930afca179f64f5a5bda4af29442208c86c1c39
David-Lyu <lyu.david@yahoo.com>
2024-01-21 20:22:39 -0800

    Started adding the form

--- wordpress/wp-content/plugins/custom-post-message/first-plugin.php
+++ wordpress/wp-content/plugins/custom-post-message/first-plugin.php
@@ -17,6 +17,17 @@ class WordCountAndTimePlugin {
     public function __construct() {
         //becuase in class second arguement is an array instead of string, with it poiting to this class and the method name
         add_action('admin_menu',[$this, 'adminPage']);
+        add_action('admin_init',[$this,'settings']);
+    }
+
+    function settings() { 
+        add_settings_section('wcp_first_section',null,null,'word-count-settings-page');
+
+        //wcp_location from register_settings, 'word-count-settings-page' is the page url, section 
+        add_settings_field('wcp_location', 'Display Location', [$this, 'locationHTML'], 'word-count-settings-page', 'wcp_first_section');
+
+        //sanitize_text_field built in wp to sanitize for us
+        register_setting('wordcountplugin','wcp_location',['sanitize_callback' => 'sanitize_text_field','default' => 0]);
     }
     
     function adminPage() {
